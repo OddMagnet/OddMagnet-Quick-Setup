883ceada0b1f5b8a17dca21c816573da19f76210
OddMagnet <mibruenen@gmail.com>
2021-11-19 15:39:20 +0100

    Fix stupid error

--- git/update.sh
+++ git/update.sh
@@ -1,4 +1,4 @@
-curl https://github.com/github/gitignore/blob/cdd9e946da421758c6f42c427c7bc65c8326155d/Global/macOS.gitignore > ~/.config/git/macOS.gitignore
-curl https://github.com/github/gitignore/blob/cdd9e946da421758c6f42c427c7bc65c8326155d/Global/Xcode.gitignore > ~/.config/git/Xcode.gitignore
-curl 'https://github.com/github/gitignore/blob/cdd9e946da421758c6f42c427c7bc65c8326155d/Swift.gitignore' > ~/.config/git/Swift.gitignore
-curl https://github.com/github/gitignore/blob/cdd9e946da421758c6f42c427c7bc65c8326155d/Objective-C.gitignore > ~/.config/git/Objective-C.gitignore
+curl -LJO https://raw.githubusercontent.com/github/gitignore/cdd9e946da421758c6f42c427c7bc65c8326155d/Global/macOS.gitignore > ~/.config/git/macOS.gitignore
+curl -LJO https://raw.githubusercontent.com/github/gitignore/cdd9e946da421758c6f42c427c7bc65c8326155d/Global/Xcode.gitignore > ~/.config/git/Xcode.gitignore
+curl -LJO https://raw.githubusercontent.com/github/gitignore/cdd9e946da421758c6f42c427c7bc65c8326155d/Swift.gitignore > ~/.config/git/Swift.gitignore
+curl -LJO https://raw.githubusercontent.com/github/gitignore/cdd9e946da421758c6f42c427c7bc65c8326155d/Objective-C.gitignore > ~/.config/git/Objective-C.gitignore
